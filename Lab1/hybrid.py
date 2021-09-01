10290abf71074b430014bd2642beb0e95aa85814
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2021-09-01 17:01:11 +0800

    Add files via upload

--- Lab1/hybrid.py
+++ Lab1/hybrid.py
@@ -1,15 +1,15 @@
 import timeit
 
-
 def main():
-    arr = open("number1000.txt").read().split()  # Replace test file here
+    arr = open("ascending10000.txt").read().split()  # Replace test file here
     for i in range(0, len(arr)):
         arr[i] = int(arr[i])
-    #print(arr)
+    print(arr)
+#     arr = [14,40,31,28,3,15,17,51]
     time_start = timeit.default_timer()
     comparisons = mergesort(arr, 0, len(arr) - 1)
     time_stop = timeit.default_timer()
-    print(arr)
+    #print(arr)
     print("Time elapsed for program: ", (time_stop - time_start) * 1000, "milliseconds")  # Time in milliseconds
     print("Number of comparisons: ", comparisons)
 
@@ -17,9 +17,9 @@ def main():
 def mergesort(arr, n, m):
     x = y = z = comparisons = 0
     mid = (n + m) // 2
-    if m - n <= 42:  # insert S here
+    if m - n < 42:  # insert S here
         z = insertionSort(arr, n, m)  # code here is changed to perform insertion sort below S elements
-    elif m - n > 42:  # insert S here
+    elif m - n >= 42:  # insert S here
         x = mergesort(arr, n, mid)
         y = mergesort(arr, mid + 1, m)
         comparisons = merge(arr, n, m)
@@ -36,7 +36,10 @@ def merge(arr, n, m):
     mid = (n + m) // 2
     left = arr[n:mid + 1]
     right = arr[mid + 1:m + 1]
+    did = False
+    
     while len(left) != 0 and len(right) != 0:
+        did = True
         comparisons+=1
         if left[0] < right[0]:
             arr[n] = left[0]
@@ -62,20 +65,28 @@ def merge(arr, n, m):
             arr[n] = right[0]
             right.pop(0)
             n += 1
-
+    
+    if did == False:
+        comparisons+=1
+        
     return comparisons
 
 
 def insertionSort(arr, n, m):
-    comparisons = 1
+    comparisons = 0
     for i in range(n, m + 1):
         key = arr[i]
         j = i - 1
+        did = False
         while j >= n and key < arr[j]:
+            did = True
             comparisons+=1
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
+        
+        if did == False:
+            comparisons+=1
 
     return comparisons
 
--- Lab1/mergesort.py
+++ Lab1/mergesort.py
@@ -2,14 +2,15 @@ import timeit
 
 
 def main():
-    arr = open("number1000.txt").read().split()     # Replace test file here
+    arr = open("ascending10000.txt").read().split()     # Replace test file here
     for i in range(0, len(arr)):
         arr[i] = int(arr[i])
-    #print(arr)
+    print(arr)
+#     arr = [23,23,23,23,23,23,23,23]
     time_start = timeit.default_timer()
     comparisons = mergesort(arr, 0, len(arr) - 1)
     time_stop = timeit.default_timer()
-    print(arr)
+    #print(arr)
     print("Time elapsed for program: ", (time_stop - time_start)*1000, "milliseconds")  # Time in milliseconds
     print("The total number of comparisons: ", comparisons)
 
@@ -18,7 +19,7 @@ def mergesort(arr, n, m):
     x = y = comparisons = 0
     mid = (n + m) // 2
     if m - n <= 0:
-        return comparisons
+        return 1
     elif m - n > 1:
         x = mergesort(arr, n, mid)
         y = mergesort(arr, mid + 1, m)
@@ -29,12 +30,16 @@ def mergesort(arr, n, m):
 def merge(arr, n, m):
     comparisons = 0
     if m - n <= 0:
-        return comparisons
+        return 1
 
     mid = (n + m) // 2
     left = arr[n:mid + 1]
     right = arr[mid + 1:m + 1]
+    
+    did = False
+    
     while len(left) != 0 and len(right) != 0:
+        did = True
         comparisons+=1
         if left[0] < right[0]:
             arr[n] = left[0]
@@ -60,7 +65,10 @@ def merge(arr, n, m):
             arr[n] = right[0]
             right.pop(0)
             n += 1
-            
+    
+    if did == False:
+        comparisons+=1
+        
     return comparisons
 
 main()
